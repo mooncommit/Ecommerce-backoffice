eb34bc2465a6c7aeb2949e88fb7a27457dce8912
Sole02 <ljaeseok02@gmail.com>
2026-04-24 20:58:49 +0900

    feat : 기본 구조 생성

---
+++ src/main/java/com/example/ecommerce_backoffice/customer/contoller/CustomerContoller.java
@@ -0,0 +1,10 @@
+package com.example.ecommerce_backoffice.customer.contoller;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Repository
+@RequestMapping("/customers")
+public class CustomerContoller {
+}
