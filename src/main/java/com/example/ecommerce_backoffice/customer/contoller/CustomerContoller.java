6153380cd39f3dead02b6dea33a43c2d5a04c1bb
Sole02 <ljaeseok02@gmail.com>
2026-04-26 20:27:02 +0900

    feat : 다건 조회 기능 구현

--- src/main/java/com/example/ecommerce_backoffice/customer/contoller/CustomerContoller.java
+++ src/main/java/com/example/ecommerce_backoffice/customer/contoller/CustomerContoller.java
@@ -1,10 +1,26 @@
 package com.example.ecommerce_backoffice.customer.contoller;
 
-import org.springframework.stereotype.Repository;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.ecommerce_backoffice.customer.dto.CustomerReadAllResponseDto;
+import com.example.ecommerce_backoffice.customer.service.CustomerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-@Repository
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
 @RequestMapping("/customers")
 public class CustomerContoller {
+
+    // 속성
+    private final CustomerService customerService;
+
+
+    // 다건 조회
+    @GetMapping
+    public ResponseEntity<List<CustomerReadAllResponseDto>> getAllCustomers() {
+        return ResponseEntity.status(HttpStatus.OK).body(customerService.getCustomers());
+    }
 }
--- src/main/java/com/example/ecommerce_backoffice/customer/service/CustomerService.java
+++ src/main/java/com/example/ecommerce_backoffice/customer/service/CustomerService.java
@@ -1,7 +1,32 @@
 package com.example.ecommerce_backoffice.customer.service;
 
+import com.example.ecommerce_backoffice.customer.dto.CustomerReadAllResponseDto;
+import com.example.ecommerce_backoffice.customer.repository.CustomerRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
+@RequiredArgsConstructor
 public class CustomerService {
+
+    private final CustomerRepository customerRepository;
+
+    // 다건 조회
+    @Transactional(readOnly = true)
+    public List<CustomerReadAllResponseDto> getCustomers() {
+        return customerRepository.findAll().stream()
+                .map(customer -> new CustomerReadAllResponseDto(
+                        customer.getId(),
+                        customer.getName(),
+                        customer.getEmail(),
+                        customer.getPhone(),
+                        customer.getStatus(),
+                        customer.getCreatedAt()
+                ))
+                .collect(Collectors.toList());
+    }
 }
