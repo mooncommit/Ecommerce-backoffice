f14542d31bb58575c6e072afb4e565d35035eba0
민병준 <minbyeongjun@minbyeongjun-ui-MacBookAir.local>
2026-04-28 17:22:02 +0900

    Entity 주석 수정

--- src/main/java/com/example/ecommerce_backoffice/product/entity/Product.java
+++ src/main/java/com/example/ecommerce_backoffice/product/entity/Product.java
@@ -25,31 +25,31 @@ public class Product extends BaseEntity {
     @JoinColumn(name = "admin_id", nullable = false)
     private Admin admin;
 
-    // 상품명
+
     @Column(nullable = false, length = 255)
     private String name;
 
-    // 카테고리 (전자기기, 패션/의류, 식품)
+
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private ProductCategory category;
 
-    // 가격
+
     @Column(nullable = false)
     private int price;
 
-    // 재고
+
     @Column(nullable = false)
     private int stock;
 
-    // 상품 상태 (ON_SALE, SOLD_OUT, DISCONTINUED)
+
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private ProductStatus status;
 
     private LocalDateTime deletedAt;
 
-    // 상품 생성자
+
     public Product(Admin admin, String name, ProductCategory category, int price, int stock, ProductStatus status) {
         this.admin = admin;
         this.name = name;
@@ -59,23 +59,23 @@ public class Product extends BaseEntity {
         this.status = status;
     }
 
-    // 상품 정보 수정 - 상품명, 카테고리, 가격만 변경
+
     public void updateInfo(String name, ProductCategory category, int price) {
         this.name = name;
         this.category = category;
         this.price = price;
     }
 
-    // 상품 재고 변경
+
     public void updateStock(int stock) {
         this.stock = stock;
 
-        // 단종 상태면 상태 유지
+
         if (this.status == ProductStatus.DISCONTINUED) {
             return;
         }
 
-        // 재고 0 이하 -> 품절 / 재고 1 이상 -> 판매중 재고에 따라 상태 자동으로 전환
+
         if (this.stock <= 0) {
             this.status = ProductStatus.SOLD_OUT;
         } else {
@@ -83,12 +83,12 @@ public class Product extends BaseEntity {
         }
     }
 
-    // 상품 상태 변경
+
     public void updateStatus(ProductStatus status) {
         this.status = status;
     }
 
-    // 주문 시 재고차감 메서드
+
     public void decreaseStock(int quantity) {
         this.stock -= quantity;
         if (this.stock <= 0) {
@@ -96,7 +96,6 @@ public class Product extends BaseEntity {
         }
     }
 
-    // 주문 취소 시 재고복구 메서드
     public void restoreStock(int quantity) {
         this.stock += quantity;
         if (this.status != ProductStatus.DISCONTINUED) {
