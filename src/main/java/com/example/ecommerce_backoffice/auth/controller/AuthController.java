c13070f3c16fd945b7cdb7f4954eea8397af4c2d
JeonYongWoon <oioipo22@gmail.com>
2026-04-30 11:07:39 +0900

    auth-response

--- src/main/java/com/example/ecommerce_backoffice/auth/controller/AuthController.java
+++ src/main/java/com/example/ecommerce_backoffice/auth/controller/AuthController.java
@@ -3,6 +3,7 @@ package com.example.ecommerce_backoffice.auth.controller;
 import com.example.ecommerce_backoffice.admin.entity.Admin;
 import com.example.ecommerce_backoffice.auth.dto.LoginRequestDto;
 import com.example.ecommerce_backoffice.auth.dto.SignupCreateRequestDto;
+import com.example.ecommerce_backoffice.common.dto.ApiResponseDto;
 import com.example.ecommerce_backoffice.common.dto.SessionAdmin;
 import com.example.ecommerce_backoffice.auth.service.AuthService;
 import jakarta.servlet.http.HttpSession;
@@ -23,15 +24,15 @@ public class AuthController {
 
     //회원가입
     @PostMapping("/signup")
-    public ResponseEntity<String> register(@Valid @RequestBody SignupCreateRequestDto request) {
+    public ResponseEntity<ApiResponseDto<String>> register(@Valid @RequestBody SignupCreateRequestDto request) {
         authService.createRegister(request);
-        return ResponseEntity.status(HttpStatus.CREATED).body("관리자등록이 완료되었습니다. 슈퍼 관리자의 승인이 있어야 로그인 가능합니다.");
+        return ResponseEntity.status(HttpStatus.CREATED).body(ApiResponseDto.success(HttpStatus.CREATED, "회원가입이 완료되었습니다. 슈퍼관리자의 승인이 있어야 로그인 가능합니다.", null));
     }
 
 
     //로그인
     @PostMapping("/login")
-    public ResponseEntity<String> adminLogin(@Valid @RequestBody LoginRequestDto request, HttpSession session ) {
+    public ResponseEntity<ApiResponseDto<String>> adminLogin(@Valid @RequestBody LoginRequestDto request, HttpSession session ) {
         Admin admin = authService.loginAdmin(request);
         SessionAdmin sessionAdmin = new SessionAdmin(
                 admin.getId(),
@@ -40,19 +41,19 @@ public class AuthController {
         );
         session.setAttribute("loginAdmin", sessionAdmin);
 
-        return ResponseEntity.ok("로그인 성공!");
+        return ResponseEntity.ok(ApiResponseDto.success(HttpStatus.OK, "로그인 되었습니다.", null));
     }
 
     @PostMapping("/logout")
-    public ResponseEntity<String> adminLogout(@SessionAttribute(name = "loginAdmin", required = false)
+    public ResponseEntity<ApiResponseDto<String>> adminLogout(@SessionAttribute(name = "loginAdmin", required = false)
                                             SessionAdmin sessionAdmin,
                                             HttpSession session) {
         if (sessionAdmin==null){
-            return ResponseEntity.status(401).body("이미 만료된 세션입니다.");
+            return ResponseEntity.status(401).body(ApiResponseDto.success(HttpStatus.UNAUTHORIZED, "이미 만료된 세션입니다.", null));
         }
 
         session.invalidate();
-        return ResponseEntity.ok("로그아웃 성공!");
+        return ResponseEntity.ok(ApiResponseDto.success(HttpStatus.OK, "로그아웃 되었습니다", null));
     }
 }
 
